d88ed09d474e7467d4249aee573f6b40d229463d
Vadym Yashchenko <vadym.yashchenko@gmail.com>
2020-05-20 19:58:08 +0300

    home task 3

---
+++ 2.js
@@ -0,0 +1,25 @@
+/**
+ * Задача 2.
+ *
+ * Напишите функцию checkSpam(source, example)
+ * Функция возвращает true, если строка source содержит подстроку example. Иначе — false.
+ *
+ * Условия:
+ * - Функция принимает два параметра;
+ * - Функция содержит валидацию входных параметров на тип string.
+ * - Функция должна быть нечувствительна к регистру:
+ */
+
+// Решение
+
+function checkSpam(source, example) {
+    let lowerStr = source.toLowerCase();
+    let lowerEx = example.toLowerCase();
+
+    return lowerStr.includes(lowerEx) || lowerStr.includes(lowerEx);
+}
+
+checkSpam('pitterXXX@gmail.com', 'xxx'); // true
+checkSpam('pitterxxx@gmail.com', 'XXX'); // true
+
+exports.checkSpam = checkSpam;
